0da07d619b255ff1fa12e8803d5678fa651d9440
Oluwaniphise <oduyaleenoch@gmail.com>
2022-06-28 10:42:24 +0100

    added search functionality

--- src/Note.jsx
+++ src/Note.jsx
@@ -9,7 +9,7 @@ const Note = ({note, notes, setNotes, id}) => {
 
                 // <div className=" mt-3 ">
               <div key={note.id} className="relative mx-3 px-[3rem] bg-yellow-400 p-2 w-auto h-auto md:w-[350px] rounded-lg">
-                <h3 className="text-black text-2xl">{note.title}</h3>
+                <h3 className="text-black title text-2xl">{note.title}</h3>
                 <p className="mb-4">{note.date} at {note.time}</p>
                 <p className="text-black mb-[1rem]">{note.text}</p>
   
